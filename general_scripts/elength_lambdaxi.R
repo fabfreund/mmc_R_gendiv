0bb1b7beb4d00acd164951deebb186a097b87da8
schonufb <fabian.freund@uni-hohenheim.de>
2019-07-12 18:08:12 +0200

    finished the expected length script

--- general_scripts/elength_lambdaxi.R
+++ general_scripts/elength_lambdaxi.R
@@ -3,8 +3,10 @@
 
 
 #Input: nmax sample size for which to compute E(Ln), ratefun Function of n that
-#gives the transition rates of the Lambda-n-coalescent
-eln_f_lambda <- function(nmax,ratefun){
+#gives the transition rates of the block counting pocess of the Lambda-n-coalescent. 
+#If s > 0 it returns the
+#Watterson estimator 2*s/E(Ln) instead.
+eln_f_lambda <- function(nmax,ratefun,s=0){
 eln <- rep(0,nmax)
 rates_l <- sapply(2:nmax,ratefun) #list of Rate vectors for all n used
 eln[2] <- 2/(sum(rates_l[[c(1,2)]])) #E(L_2)=2*E(T2)
@@ -19,7 +21,8 @@ for (i in 3:nmax){
     #trans probs = rates/totalrate
     p_i <- p_i/(sum(rates1))
     #Recursion
-    eln[i] <- i/(sum(rates1)) + sum(eln[1:(i-1)]*p_i)}  
+    eln[i] <- i/(sum(rates1)) + sum(eln[1:(i-1)]*p_i)}
+  if (s>0){eln[nmax] <- 2*s/eln[nmax]}
   return(eln[nmax])  
 }
 
@@ -42,8 +45,11 @@ for (i in seq(along=alpha_range)){
 #' Compute E(Ln) for the BetaXi n-coalescent
 #' Input: nmax sample size for which to compute E(Ln), ratefun beta4xi_rates
 #' from script betaxicoal_sim.R as a function of n, e.g. function(n){beta4xi_rates(n,0.5,1.5)}
-#' for BetaXi with params a=0.5,b=1.5
-eln_f_xi <- function(nmax,ratefun){
+#' for BetaXi with params a=0.5,b=1.5.
+#If s > 0 it returns the
+#Watterson estimator 2*s/E(Ln) instead.
+
+eln_f_xi <- function(nmax,ratefun,s=0){
   eln <- rep(0,nmax)
   rates_l <- sapply(2:nmax,ratefun) #Rate vectors for all n used
   eln[2] <- 2/(sum(rates_l[[c(1,2)]])) #E(L_2)=2*E(Exp(0.25))
@@ -61,5 +67,6 @@ eln_f_xi <- function(nmax,ratefun){
     #trans probs = rates/totalrate
     p_i <- p_i/(sum(rates1[[2]]))
     eln[i] <- i/(sum(rates1[[2]])) + sum(eln[1:(i-1)]*p_i)}  
+  if (s>0){eln[nmax] <- 2*s/eln[nmax]}
   return(eln[nmax])
 }

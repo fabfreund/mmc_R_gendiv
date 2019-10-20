dab25aed78682d210e429710d0c1bb8721c29df9
schonufb <fabian.freund@uni-hohenheim.de>
2019-10-20 17:03:50 +0200

    rewritten ABC via Rscript, test model-misspec

---
+++ distpaper_res/abcinput_stand_stats.R
@@ -0,0 +1,37 @@
+#' Name of simulated data (without sim_ and .RData) has to be both in sims_rep1 and sims_rep2)
+#dataname <- "m123_af_n100" # New version: is provided as Rscript argument
+seed1 <- 46611 #Seed 14, use the same for all such ABCs
+
+#' Rows that are kept for each set of statistics used for model selection
+o_coarse <- 1:8
+ham_coarse <- 9:13
+phy_coarse <- 14:18
+r2_coarse <-  19:23
+s_pi <- c(24,25)
+af_coarse <- 26:30
+rows_abc <- list(c(o_coarse,ham_coarse,phy_coarse,r2_coarse,s_pi,af_coarse),
+                 c(ham_coarse,phy_coarse),
+                 c(o_coarse,r2_coarse),
+                 c(s_pi,af_coarse),
+                 c(ham_coarse,phy_coarse,r2_coarse,s_pi,af_coarse),
+                 c(o_coarse,r2_coarse,s_pi,af_coarse),
+                 c(o_coarse,ham_coarse,phy_coarse,s_pi,af_coarse),
+                 c(o_coarse,ham_coarse,phy_coarse,r2_coarse),
+                 c(ham_coarse,phy_coarse,s_pi,af_coarse))
+
+
+
+#' Input vector
+stats1 <- vector("list",9)
+
+#' Names of sets of stats
+names(stats1) <- c("FULL","Ham, Phy","O, r2","AF+",
+                   "FULL - O","O, r2, AF+","FULL - r2","FULL - AF+","Ham,Phy,AF+")
+
+
+#' How many ABC runs per set of stats per replication
+nreps <- 10
+#'Output objects: oob errors and varuiable importances
+res_conf <- vector("list",9)
+names(res_conf) <- names(stats1)
+res_imp <- vector("list",nreps)
--- distpaper_res/mA_abcrfvsmBC.R
+++ distpaper_res/mA_abcrfvsmBC.R
@@ -1,7 +1,10 @@
-args <- commandArgs(TRUE) # 1: filename_short, 2: model to test 3:,4: models to build forest off
-
+#' Use Rscript with 4+ arguments.  1: sims name (w/o sims_ and .RData), contains 
+#' ALL sims needed (see next arguments) 
+#' 2: model to test 3:,4:,... models to build forest off
+args <- commandArgs(TRUE)
 
 data_path <- paste0("sims_rep1/sim_",args[1],".RData")
+#' mod1: this model's simulations will get sorted via a RF built on models mods_rf
 mod1 <- as.numeric(args[2])
 mods_rf <- as.numeric(args[-c(1,2)])
 
@@ -9,34 +12,39 @@ library(abcrf)
 
 load(data_path)
 
+#' Safety: Sims should not have missing data...
 bad_cols <- apply(sims1,2,function(v){any(is.na(v))})
-bad_cols <- bad_cols #| (prior1[,4]<=10)
+bad_cols <- bad_cols 
+
 
 sims1 <- sims1[,!(bad_cols)]
 model1 <- as.factor(prior1[!(bad_cols),2])
 
+#' sims for building decision RF
 prior_m_rf <- which(model1 %in% mods_rf)
 sims_m_rf <- sims1[,prior_m_rf]
 model_m_rf <- droplevels(model1[prior_m_rf])
+stats_m_rf <- data.frame(model_m_rf,t(sims_m_rf)) 
 
+#' sims to allocate w. RF
 prior_mtest <- which(model1==mod1)
 sims_mtest <- sims1[,prior_mtest]
 model_mtest <- droplevels(model1[prior_mtest])
-
-
-stats_m_rf <- data.frame(model_m_rf,t(sims_m_rf)) 
 stats_mtest <- data.frame(model_mtest,t(sims_mtest))
 
 
+#' Kill stats that have no variation 
 posvar_rvs <- 1+which(apply(stats_m_rf[,-1],2,sd)>0)
 stay_cols <- c(1,posvar_rvs)
 
+#' Build RF for model selection
 rf_m_rf <- abcrf(model_m_rf~.,data=stats_m_rf[,stay_cols],ntree=1000,paral = TRUE,lda=FALSE)
+#' For all sims of model mod1, allocate it to the other models via RF rf_m_rf
 pred_m_rf <- predict(rf_m_rf,stats_mtest[,posvar_rvs],stats_m_rf[,stay_cols],paral = TRUE)
 rm(rf_m_rf)
 
-
-save(pred_m_rf,file=paste0("pred_m",mod1,"vsrest_",args[1],".RData"))
+#' save model allocation result
+save(pred_m_rf,file=paste0("mismod_res/pred_m",mod1,"vsrest_",args[1],".RData"))
 
 
 
